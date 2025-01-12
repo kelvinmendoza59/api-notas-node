0841bc560088f8a7303e51e383b6f54c2d1ee5f6
Kelvin Mendoza <kelvinmendoza573@gmail.com>
2025-01-12 10:20:00 -0500

    Implement CORS middleware

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
@@ -90,7 +98,6 @@ app.delete('/api/notas/:id', (req, res) => {
     res.status(204).send();
 });
 
-// TODO: Add CORS middleware for production
 // TODO: Add rate limiting for API endpoints
 
 app.listen(PORT, () => {
